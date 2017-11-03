ceb17c03d30edc4723ab8c75c5d3515086aadcf3
sarsatis <sarthak.satish@capgemini.com>
2017-11-03 11:11:08 +0000

    Added File

--- HelloWorld/src/main/java/com/example/controller/HelloWorld.java
+++ HelloWorld/src/main/java/com/example/controller/HelloWorld.java
@@ -1,10 +1,11 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-	
+	@GetMapping("/")
 	public String sayHello() {
 		return "hello";
 	}
